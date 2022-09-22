73ff6d7d41ba86f898dcc6ec8eb32989602850db
linxiaowang <40813807+linxiaowang@users.noreply.github.com>
2022-09-22 16:01:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oUHud6Wszt4_ylxDlkDoaLjKk6eI",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "IOLXKVk-FWaqaIj4Oi1HG9_S9kfD8Lz7r9ZHJ-09Vk0",
       // 所在省份
       province: "广东",
       // 所在城市
